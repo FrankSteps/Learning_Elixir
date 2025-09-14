05f3fc3f0f743c6db58cfa075a69e5a5e1b1ac19
FrankSteps <franciscopassos.contato@gmail.com>
2025-09-13 21:01:54 -0300

    very eeeaaasssyyyy 🦅🦅🦅🦅🦅🦅🦅

---
+++ challenger/001_calculator.exs
@@ -0,0 +1,22 @@
+# calculator in elixir!!!!
+
+x = IO.gets("x = ") |> String.trim() |> String.to_integer();
+y = IO.gets("y = ") |> String.trim() |> String.to_integer();
+
+symbol = IO.gets("put your operation (+, -, * or /): ") |> String.trim();
+
+cond do
+  symbol === "+" -> IO.puts(x + y);
+  symbol === "-" -> IO.puts(x - y);
+  symbol === "*" -> IO.puts(x * y);
+  symbol === "/" and y != 0 -> IO.puts(x / y);
+  symbol === "/" and y == 0 ->
+    IO.puts("error -> infinity");
+    System.halt(0);
+  true - > IO.puts("invalid operation")
+end;
+
+System.halt(0);
+
+#EASY
+#VEEERRRRRYYYYYYYY EEEAAAAAAAAASSSSSSYYYYYYYYY 🦅🦅🦅🦅🦅🦅🦅🦅
